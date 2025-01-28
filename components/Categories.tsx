03e8e63d66e8616f6945dd47a69d475e207bb499
ntracie <naluwaggatracy587@gmail.com>
2025-01-28 12:19:56 +0300

    card

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -4,9 +4,9 @@ export default function Categories() {
   return (
     <div>
       <section>
-      <div className='image3 w-[900] h-[900]'>
-        <h3 className='font-bold text-3xl'>SPECIAL OFFERS ON <br />Cough Syrup</h3>
-        <button className='bg-blue-300 p-3'>show more</button>
+        <div className='image3 w-[900] h-[900]'>
+          <h3 className='font-bold text-3xl'>SPECIAL OFFERS ON <br />Cough Syrup</h3>
+          <button className='bg-blue-300 p-3'>show more</button>
         </div>
       </section>
         
